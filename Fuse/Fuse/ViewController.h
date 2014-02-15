90c8efb260474ced55f33ad1947064f7f3f08a26
Carlos Folgar <folgar.tech@gmail.com>
2014-02-15 15:29:12 -0500

    Added multipeer connectivity
    it finds local people now

--- Fuse/Fuse/ViewController.h
+++ Fuse/Fuse/ViewController.h
@@ -7,7 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MCNearbyServiceAdvertiserDelegate, UIActionSheetDelegate,
+MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCBrowserViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *rxLabel;
+@property (weak, nonatomic) IBOutlet UIButton *browseButton;
+@property (weak, nonatomic) IBOutlet UIButton *sendButton;
+
+@property (weak, nonatomic) NSMutableArray * mutableBlockedPeers;
+
+
+@property (nonatomic, strong) NSArray *lastResultArray;
+@property (nonatomic, strong) MCBrowserViewController *browserVC;
+@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
+@property (nonatomic, strong) MCNearbyServiceBrowser *nearbyBrowser;
+@property (nonatomic, strong) MCSession *mySession;
+@property (nonatomic, strong) MCPeerID *myPeerID;
+
+@property (strong, nonatomic) NSString * resultMsg;
+
+
 
 @end
--- Fuse/Fuse/ViewController.m
+++ Fuse/Fuse/ViewController.m
@@ -17,7 +17,17 @@
 
 @end
 
+static NSString * const CardsServiceType = @"cards-service";
+
+
 @implementation ViewController
+
+
+- (void)browser:(MCNearbyServiceBrowser *)browser foundPeer:(MCPeerID *)peerID withDiscoveryInfo:(NSDictionary *)info
+{
+    NSLog(@"browser found peer %@ with discovery info : %@", peerID.displayName, info);
+}
+
 
 - (void)viewDidLoad
 {
@@ -33,9 +43,126 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setup
+{
+    
+    NSLog(@"ViewController.m setup method called because awakeFromNib.");
+    // setup peer ID
+    self.myPeerID = [[MCPeerID alloc] initWithDisplayName:[[UIDevice currentDevice] name]];
+    
+    // setup session
+    self.mySession = [[MCSession alloc] initWithPeer:self.myPeerID];
+    self.mySession.delegate = self;
+    
+    self.nearbyBrowser = [[MCNearbyServiceBrowser alloc] initWithPeer:self.myPeerID serviceType:CardsServiceType];
+    self.nearbyBrowser.delegate = self;
+    [self.nearbyBrowser startBrowsingForPeers];
+    
+    // setup advertiser
+    self.advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:CardsServiceType discoveryInfo:nil session:self.mySession];
+    [self.advertiser start];
+    
+    //[self.browseButton addTarget:self action:@selector(showBrowserVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.sendButton addTarget:self action:@selector(sendText) forControlEvents:UIControlEventTouchUpInside];
+
+    
+}
+
+
+-(void)awakeFromNib
+{
+    NSLog(@"called awakeFromNib");
+    [self setup];
+}
+
 - (IBAction)test1:(id)sender {
 }
 
 - (IBAction)test2:(id)sender {
 }
+
+
+-(void)viewWillAppear:(BOOL)animated
+{
+//    [self.browseButton addTarget:self action:@selector(showBrowserVC) forControlEvents:UIControlEventTouchUpInside];
+    [self.sendButton addTarget:self action:@selector(sendText) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+}
+
+
+
+- (void) receiveMessage: (NSString *) message fromPeer: (MCPeerID *) peer
+{
+    NSLog(@"Calling receiveMessage: fromPeer: method");
+    self.resultMsg = [message copy];
+    self.rxLabel.text = self.resultMsg;
+    [self.rxLabel setNeedsDisplay];
+    
+}
+
+- (void) sendText
+{
+    NSLog(@"Calling sendText method");
+    NSString *message = [[UIDevice currentDevice] name];
+    //  Convert text to NSData
+    NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
+    //  Send data to connected peers
+    NSError *error;
+    [self.mySession sendData:data toPeers:[self.mySession connectedPeers] withMode:MCSessionSendDataUnreliable error:&error];
+}
+
+// presents the VC that shows nearby beacons
+//-(void) showBrowserVC
+//{
+//    NSLog(@"attempting to showBrowserVC");
+//    [self presentViewController:self.browserVC animated:YES completion:nil];
+//}
+
+// dismisses VC presented above
+//- (void) dismissBrowserVC { [self.browserVC dismissViewControllerAnimated:YES completion:nil]; }
+
+#pragma marks MCBrowserViewControllerDelegate
+
+// Notifies the delegate, when the user taps the done button
+//- (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController { [self dismissBrowserVC]; }
+
+// Notifies delegate that the user taps the cancel button.
+//- (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController { [self dismissBrowserVC]; }
+
+#pragma marks MCSessionDelegate
+// Remote peer changed state
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state {}
+
+// Received a byte stream from remote peer
+- (void)session:(MCSession *)session didReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID {}
+
+// Start receiving a resource from remote peer
+- (void)session:(MCSession *)session didStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress {}
+
+// Finished receiving a resource from remote peer and saved the content in a temporary location - the app is responsible for moving the file to a permanent location within its sandbox
+- (void)session:(MCSession *)session didFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(NSError *)error {}
+
+
+/*  First endpoint for data being received. Routes it over to receiveMessage:fromPeer: */
+- (void) session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID
+{
+    //  Decode data back to NSString
+    NSString *message = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    //  append message to text box on main thread
+    dispatch_async(dispatch_get_main_queue(),^{
+        [self receiveMessage: message fromPeer: peerID];
+    });
+}
+
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSLog(@"performing push segue...");
+}
+
+
 @end
