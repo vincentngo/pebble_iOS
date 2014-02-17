9b7b716b113b681cd260b80bb78b61255771a08f
vincentngo <vincentn@vt.edu>
2014-02-17 01:28:14 -0500

    Edit Author

--- Fuse/Fuse/AppDelegate.h
+++ Fuse/Fuse/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  Fuse
 //
-//  Created by Vincent Ngo on 2/15/14.
-//  Copyright (c) 2014 Vincent Ngo. All rights reserved.
+//  Created by Vincent Ngo and Carlos Folgar on 2/15/14.
+//  Copyright (c) 2014 Vincent Ngo and Carlos Folgar. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
